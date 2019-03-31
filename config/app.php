ff0c3b4d731ab26ad1fdf9694b0c222b98287db9
Jcaltamare <Jcaltamare@gmail.com>
2019-03-31 13:17:31 -0500

    5.8

--- config/app.php
+++ config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         LaravelFCM \ FCMServiceProvider :: class ,
+        Appointer\Swaggervel\SwaggervelServiceProvider::class,
+
 
 
     ],
