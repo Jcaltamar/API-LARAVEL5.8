af3e62786d7a481a9395d1dc85b8f4b38551adf2
Jcaltamare <Jcaltamare@gmail.com>
2019-04-02 09:16:20 -0500

    trustedpro

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index');
 
 Route::resource('noticias', 'NoticiaController');
 
 Route::resource('novedades', 'NovedadesController');
-
-Route::resource('noticias', 'NoticiaController');
